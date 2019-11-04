db0284ed019c4cf2c9829c2d3bf0ee4ff00f4ef9
Caio Silva <bladellano@gmail.com>
2019-11-04 06:10:21 -0300

    Métodos para strings

--- aula12/aula12.js
+++ aula12/aula12.js
@@ -7,12 +7,19 @@ let varC = 'C';
 varA = varB;
 varB = varC;
 varC = varATemp; */
-
 console.log(varA, varB, varC);
 
 let umaString = "Um texto";
-console.log(umaString.charAt(6));
-console.log(umaString.charCodeAt(6)); //Procura na tabela asc
-console.log(umaString.indexOf('texto'));
-console.log(umaString.lastIndexOf('m',3));
+let outraString = "O rato roeu a roupa do rei de roma.";
+console.log('chartAt:',umaString.charAt(6));
+console.log('CharCodeAt:',umaString.charCodeAt(6)); //Procura na tabela asc
+console.log('indexOf:',umaString.indexOf('texto'));
+console.log('lastIndexOf:',umaString.lastIndexOf('m',3));
 console.log(umaString.match(/[a-z]/g));
+console.log('search:',umaString.search(/x/));
+console.log('replace:',umaString.replace('Um','Dois'));
+console.log('replace:',outraString.replace(/r/g,'#'));
+console.log('slice:',outraString.slice(2,6));//rato
+console.log('slice:',outraString.slice(-5));//roma
+console.log('split:',outraString.split(' '));//roma
+console.log('split:',outraString.split(' ',2));//roma
